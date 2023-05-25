14dce8aa84bbf0ac728cb12cd3ae12025b656bbf
landtry <landry@boldist.co>
2023-05-24 23:34:55 -0500

    Created ACF documentation page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -184,26 +184,27 @@ function NavigationGroup({ group, className }) {
 }
 
 export const navigation = [
+  // {
+  //   title: 'Guides',
+  //   links: [
+  //     { title: 'Introduction', href: '/' },
+  //     { title: 'Quickstart', href: '/quickstart' },
+  //     { title: 'SDKs', href: '/sdks' },
+  //     { title: 'Authentication', href: '/authentication' },
+  //     { title: 'Pagination', href: '/pagination' },
+  //     { title: 'Errors', href: '/errors' },
+  //     { title: 'Webhooks', href: '/webhooks' },
+  //   ],
+  // },
   {
-    title: 'Guides',
+    title: 'Wordpress Development Options',
     links: [
-      { title: 'Introduction', href: '/' },
-      { title: 'Quickstart', href: '/quickstart' },
-      { title: 'SDKs', href: '/sdks' },
-      { title: 'Authentication', href: '/authentication' },
-      { title: 'Pagination', href: '/pagination' },
-      { title: 'Errors', href: '/errors' },
-      { title: 'Webhooks', href: '/webhooks' },
-    ],
-  },
-  {
-    title: 'Resources',
-    links: [
-      { title: 'Contacts', href: '/contacts' },
-      { title: 'Conversations', href: '/conversations' },
-      { title: 'Messages', href: '/messages' },
-      { title: 'Groups', href: '/groups' },
-      { title: 'Attachments', href: '/attachments' },
+      { title: 'Template Based Architecture', href: '/acf' },
+      // { title: 'Contacts', href: '/contacts' },
+      // { title: 'Conversations', href: '/conversations' },
+      // { title: 'Messages', href: '/messages' },
+      // { title: 'Groups', href: '/groups' },
+      // { title: 'Attachments', href: '/attachments' },
     ],
   },
 ]
